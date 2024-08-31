ae87c895af2a03142fd7f9e80966fcb46bd4cda3
Mohammad AbuJaber <mohammad.abujaber@beyondkode.com>
2024-08-31 12:59:29 +0300

    Fixing the sizing and label position of terms and conditions

--- VMAgent/frmNewSimTermsAndConditions.designer.vb
+++ VMAgent/frmNewSimTermsAndConditions.designer.vb
@@ -40,26 +40,25 @@ Partial Class frmNewSimTermsAndConditions
         Me.pnlWA.Controls.Add(Me.btnOK)
         Me.pnlWA.Controls.Add(Me.pnlMsisdns)
         Me.pnlWA.Location = New System.Drawing.Point(0, 0)
+        Me.pnlWA.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.pnlWA.Name = "pnlWA"
-        Me.pnlWA.Size = New System.Drawing.Size(1920, 1575)
+        Me.pnlWA.Size = New System.Drawing.Size(1280, 1024)
         Me.pnlWA.TabIndex = 17
         '
         'btnBack
         '
         Me.btnBack.BackColor = System.Drawing.Color.Transparent
-        Me.btnBack.Location = New System.Drawing.Point(253, 994)
-        Me.btnBack.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.btnBack.Location = New System.Drawing.Point(169, 646)
         Me.btnBack.Name = "btnBack"
-        Me.btnBack.Size = New System.Drawing.Size(698, 108)
+        Me.btnBack.Size = New System.Drawing.Size(465, 70)
         Me.btnBack.TabIndex = 16
         '
         'btnOK
         '
         Me.btnOK.BackColor = System.Drawing.Color.Transparent
-        Me.btnOK.Location = New System.Drawing.Point(970, 992)
-        Me.btnOK.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.btnOK.Location = New System.Drawing.Point(647, 645)
         Me.btnOK.Name = "btnOK"
-        Me.btnOK.Size = New System.Drawing.Size(695, 119)
+        Me.btnOK.Size = New System.Drawing.Size(463, 77)
         Me.btnOK.TabIndex = 15
         '
         'pnlMsisdns
@@ -67,31 +66,33 @@ Partial Class frmNewSimTermsAndConditions
         Me.pnlMsisdns.AutoScroll = True
         Me.pnlMsisdns.BackColor = System.Drawing.Color.Transparent
         Me.pnlMsisdns.Controls.Add(Me.RichTextBox1)
-        Me.pnlMsisdns.Location = New System.Drawing.Point(253, 339)
-        Me.pnlMsisdns.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.pnlMsisdns.Location = New System.Drawing.Point(169, 220)
+        Me.pnlMsisdns.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.pnlMsisdns.Name = "pnlMsisdns"
-        Me.pnlMsisdns.Size = New System.Drawing.Size(1412, 604)
+        Me.pnlMsisdns.Size = New System.Drawing.Size(941, 393)
         Me.pnlMsisdns.TabIndex = 14
         '
         'RichTextBox1
         '
         Me.RichTextBox1.Font = New System.Drawing.Font("Nirmala UI Semilight", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RichTextBox1.Location = New System.Drawing.Point(0, 3)
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 2)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RichTextBox1.Name = "RichTextBox1"
         Me.RichTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
-        Me.RichTextBox1.Size = New System.Drawing.Size(1409, 598)
+        Me.RichTextBox1.Size = New System.Drawing.Size(941, 390)
         Me.RichTextBox1.TabIndex = 0
         Me.RichTextBox1.Text = resources.GetString("RichTextBox1.Text")
         '
         'frmNewSimTermsAndConditions
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(1920, 1575)
+        Me.ClientSize = New System.Drawing.Size(1280, 921)
         Me.Controls.Add(Me.pnlWA)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "frmNewSimTermsAndConditions"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.TopMost = True
